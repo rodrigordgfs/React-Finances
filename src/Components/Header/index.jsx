f21837cde8fca40157213d0e3ec12a3cc667c16d
Rodrigo Viegas Rodrigues <rodrigoviegas@lojaspompeia.com.br>
2022-07-11 19:49:01 -0300

    Projeto criado

---
+++ src/Components/Header/index.jsx
@@ -0,0 +1,7 @@
+import styles from './index.module.css'
+
+export default function Header() {
+  return (
+    <div>Header</div>
+  )
+}
---
+++ src/Pages/Home/index.jsx
@@ -0,0 +1,11 @@
+
+import styles from './index.module.css'
+
+function App() {
+  return (
+    <div className="App">
+    </div>
+  )
+}
+
+export default App

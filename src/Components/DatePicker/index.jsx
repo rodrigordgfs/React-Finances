b6d14db52237bdd74d3c785cdfc6b00e4732dade
Rodrigo Viegas Rodrigues <rodrigoviegas@lojaspompeia.com.br>
2022-07-12 14:05:56 -0300

    Adicionado botao ao invez de div no componente DatePicker

--- src/Components/DatePicker/index.jsx
+++ src/Components/DatePicker/index.jsx
@@ -24,19 +24,19 @@ export default function DatePicker() {
 
   return (
     <div className="bg-zinc-800 flex flex-row flex-1 items-center rounded h-14 shadow">
-      <div
+      <button
         onClick={handleDecreaseMonth}
         className="ml-4 p-2 cursor-pointer rounded-full hover:bg-zinc-700 transition-all"
       >
         <IoMdArrowDropleft color="white" size={28} />
-      </div>
+      </button>
       <p className="flex-1 font-poppins text-zinc-300 text-center">{date}</p>
-      <div
+      <button
         onClick={handleIncreaseMonth}
         className="mr-4 p-2 rounded-full cursor-pointer hover:bg-zinc-700 transition-all"
       >
         <IoMdArrowDropright size={28} color="white" />
-      </div>
+      </button>
     </div>
   );
 }

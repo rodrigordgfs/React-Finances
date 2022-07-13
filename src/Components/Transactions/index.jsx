66024c644937c7e593f74a9d77a6317b765cbb09
Rodrigo Viegas Rodrigues <rodrigoviegas@lojaspompeia.com.br>
2022-07-13 11:25:18 -0300

    Reformulação do projeto

--- src/Components/Transactions/index.jsx
+++ src/Components/Transactions/index.jsx
@@ -1,79 +1,52 @@
-import { useContext, useEffect, useRef } from "react";
 import { AiOutlinePlus } from "react-icons/ai";
 import { HiOutlineEmojiSad } from "react-icons/hi";
-import { getTransactions } from "../../contexts/TransactionsProvider/actions";
-import { TransactionsContext } from "../../contexts/TransactionsProvider/context";
 import Transaction from "../Transaction";
 import TransactionsLoading from "../TransactionsLoading";
 
-export default function Transactions() {
-  const isMounted = useRef(true);
-  const transactionsContext = useContext(TransactionsContext);
-  const { transactionsState, transactionsDispatch } = transactionsContext;
-  const { loading } = transactionsState;
-  const transactions = transactionsState.data.transactions || [];
-
+export default function Transactions({ transactions = [] }) {
   const quantityTransactions = () => transactions.length;
 
-  useEffect(() => {
-    getTransactions(transactionsDispatch).then((dispatch) => {
-      if (isMounted.current) {
-        dispatch();
-      }
-    });
-  }, [transactionsDispatch]);
-
   return (
-    <>
-      {loading ? (
-        <TransactionsLoading />
-      ) : (
-        <>
-          <div className="flex flex-col max-w-5xl my-0 mx-auto px-2 pt-6">
-            <div className="flex flex-row items-center space-x-2">
-              <AiOutlinePlus color="white" />
-              <p className="font-poppins text-white">Minhas Transações</p>
-            </div>
-            <div className="bg-zinc-800 rounded shadow my-4 px-4 pt-4 pb-8 flex flex-col gap-4">
-              {quantityTransactions() <= 0 && (
-                <div className="flex flex-col items-center justify-center">
-                  <HiOutlineEmojiSad size={42} color="white" />
-                  <p className="font-poppins text-white fonr-semibold text-lg">
-                    Nenhum item foi encontrado neste mês!
-                  </p>
-                </div>
-              )}
-              {quantityTransactions() > 0 && (
-                <>
-                  <p className="font-poppins text-zinc-300 text-center">
-                    Nesse mês foi cadastrado{" "}
-                    <span className="font-semibold text-white">
-                      {quantityTransactions()}
-                    </span>{" "}
-                    itens.
-                  </p>
-                  <div className="space-y-5">
-                    {transactions.map((transaction) => {
-                      const { amount, category, date, id, title, type } =
-                        transaction;
-                      return (
-                        <Transaction
-                          key={id}
-                          title={title}
-                          category={category}
-                          type={type}
-                          value={amount}
-                          date={date}
-                        />
-                      );
-                    })}
-                  </div>
-                </>
-              )}
-            </div>
+    <div className="flex flex-col max-w-5xl my-0 mx-auto px-2 pt-6">
+      <div className="flex flex-row items-center space-x-2">
+        <AiOutlinePlus color="white" />
+        <p className="font-poppins text-white">Minhas Transações</p>
+      </div>
+      <div className="bg-zinc-800 rounded shadow my-4 px-4 pt-4 pb-8 flex flex-col gap-4">
+        {quantityTransactions() <= 0 ? (
+          <div className="flex flex-col items-center justify-center">
+            <HiOutlineEmojiSad size={42} color="white" />
+            <p className="font-poppins text-white fonr-semibold text-lg">
+              Nenhum item foi encontrado neste mês!
+            </p>
           </div>
-        </>
-      )}
-    </>
+        ) : (
+          <>
+            <p className="font-poppins text-zinc-300 text-center">
+              Nesse mês foi cadastrado{" "}
+              <span className="font-semibold text-white">
+                {quantityTransactions()}
+              </span>{" "}
+              itens.
+            </p>
+            <div className="space-y-5">
+              {transactions.map((transaction) => {
+                const { amount, category, date, id, title, type } = transaction;
+                return (
+                  <Transaction
+                    key={id}
+                    title={title}
+                    category={category}
+                    type={type}
+                    value={amount}
+                    date={date}
+                  />
+                );
+              })}
+            </div>
+          </>
+        )}
+      </div>
+    </div>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,12 @@ import App from './Pages/Home'
 import './styles/reset.css'
 import './styles/tailwind.css'
 import './styles/global.css'
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    <ToastContainer />
   </React.StrictMode>
 )

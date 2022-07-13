fde84636f7bb7c7480b8931fb1884e76afa9f723
Restyled.io <commits@restyled.io>
2022-07-13 16:05:46 +0000

    Restyled by prettier

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -1,13 +1,13 @@
-import Logo from '../../Components/Logo'
-import User from '../User'
+import Logo from "../../Components/Logo";
+import User from "../User";
 
 export default function Header() {
   return (
-    <div className='text-zinc-50 flex flex-col px-2 py-6'>
-      <div className='max-w-5xl w-full my-0 mx-auto flex flex-row items-center justify-between'>
+    <div className="text-zinc-50 flex flex-col px-2 py-6">
+      <div className="max-w-5xl w-full my-0 mx-auto flex flex-row items-center justify-between">
         <Logo />
         <User name="Rodrigo" />
       </div>
     </div>
-  )
+  );
 }
--- src/utils/toast.js
+++ src/utils/toast.js
@@ -1,51 +1,51 @@
-import {toast} from "react-toastify";
+import { toast } from "react-toastify";
 
 function successMessage(message = "Mensagem") {
   toast.success(message, {
-    position : "top-right",
-    autoClose : 5000,
-    hideProgressBar : false,
-    closeOnClick : true,
-    pauseOnHover : true,
-    draggable : true,
-    progress : undefined,
+    position: "top-right",
+    autoClose: 5000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
   });
 }
 
 function errorMessage(message = "Mensagem") {
   toast.error(message, {
-    position : "top-right",
-    autoClose : 5000,
-    hideProgressBar : false,
-    closeOnClick : true,
-    pauseOnHover : true,
-    draggable : true,
-    progress : undefined,
+    position: "top-right",
+    autoClose: 5000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
   });
 }
 
 function warningMessage(message = "Mensagem") {
   toast.warning(message, {
-    position : "top-right",
-    autoClose : 5000,
-    hideProgressBar : false,
-    closeOnClick : true,
-    pauseOnHover : true,
-    draggable : true,
-    progress : undefined,
+    position: "top-right",
+    autoClose: 5000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
   });
 }
 
 function infoMessage(message = "Mensagem") {
   toast.info(message, {
-    position : "top-right",
-    autoClose : 5000,
-    hideProgressBar : false,
-    closeOnClick : true,
-    pauseOnHover : true,
-    draggable : true,
-    progress : undefined,
+    position: "top-right",
+    autoClose: 5000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
   });
 }
 
-export {successMessage, errorMessage, warningMessage, infoMessage};
+export { successMessage, errorMessage, warningMessage, infoMessage };

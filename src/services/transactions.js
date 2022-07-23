32cbea09059774bba408857cd65ef824a4aaffc7
Restyled.io <commits@restyled.io>
2022-07-23 14:46:34 +0000

    Restyled by clang-format

--- src/services/transactions.js
+++ src/services/transactions.js
@@ -3,14 +3,13 @@ import Axios from "../adapters/axios";
 class TransactionsService extends Axios {
   constructor() {
     super({
-      url: "transactions",
-      config: {
+      url : "transactions",
+      config : {
         // baseURL: "https://api-finance-react.herokuapp.com/",
-        baseURL: "http://localhost:8888/",
+        baseURL : "http://localhost:8888/",
       },
     });
   }
 }
 
 export default new TransactionsService();
- 

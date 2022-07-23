726af4ab7e134a898d1628e3ec926358d4bf7ccc
Rodrigo Viegas Rodrigues <rodrigoviegas@lojaspompeia.com.br>
2022-07-22 21:01:21 -0300

    Adicionado Rotas

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import App from "./Pages/Home";
+import Router from "./Routes";
 import "./styles/global.css";
 import "./styles/reset.css";
 import "./styles/tailwind.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <Router />
     <ToastContainer />
   </React.StrictMode>
 );

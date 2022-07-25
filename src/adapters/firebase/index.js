454adcb4ec3845923bf1deb646688ac4b1c74996
Restyled.io <commits@restyled.io>
2022-07-25 14:37:55 +0000

    Restyled by clang-format

--- src/adapters/firebase/index.js
+++ src/adapters/firebase/index.js
@@ -1,15 +1,16 @@
-import { initializeApp } from "firebase/app";
-import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
-import { errorMessage } from "../../utils/toastify";
+import {initializeApp} from "firebase/app";
+import {getAuth, GoogleAuthProvider, signInWithPopup} from "firebase/auth";
+
+import {errorMessage} from "../../utils/toastify";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB3aDOWE8CcjZ7DKLcVLuok49IqhLu3mSY",
-  authDomain: "finance-e4c66.firebaseapp.com",
-  projectId: "finance-e4c66",
-  storageBucket: "finance-e4c66.appspot.com",
-  messagingSenderId: "521272776961",
-  appId: "1:521272776961:web:142570dc909cf8e8956300",
-  measurementId: "G-KNNP1PQC1X",
+  apiKey : "AIzaSyB3aDOWE8CcjZ7DKLcVLuok49IqhLu3mSY",
+  authDomain : "finance-e4c66.firebaseapp.com",
+  projectId : "finance-e4c66",
+  storageBucket : "finance-e4c66.appspot.com",
+  messagingSenderId : "521272776961",
+  appId : "1:521272776961:web:142570dc909cf8e8956300",
+  measurementId : "G-KNNP1PQC1X",
 };
 
 const app = initializeApp(firebaseConfig);
@@ -18,11 +19,11 @@ export const auth = getAuth(app);
 const provider = new GoogleAuthProvider();
 
 export const signInWithGoogle = () =>
-  signInWithPopup(auth, provider)
-    .then((result) => {
-      localStorage.setItem("user", JSON.stringify(result.user));
-    })
-    .catch((error) => errorMessage(error));
+    signInWithPopup(auth, provider)
+        .then((result) => {
+          localStorage.setItem("user", JSON.stringify(result.user));
+        })
+        .catch((error) => errorMessage(error));
 
 export const isLogedIn = () => {
   const user = JSON.parse(localStorage.getItem("user"));
